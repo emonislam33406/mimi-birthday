bf5f18e345dd77cb2b0b4598fbab854e415ba9b9
emonislam33406 <mstparvinbegum8523@gmail.com>
2026-07-09 11:28:37 +0600

    Add function to open gift and display surprise

---
+++ script.js
@@ -0,0 +1,12 @@
+function openGift(){
+
+document.querySelector(".hero").style.display="none";
+
+document.getElementById("surprise").style.display="block";
+
+window.scrollTo({
+top:0,
+behavior:"smooth"
+});
+
+}
