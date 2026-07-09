735100383f1621272cd1649024c4fb6d052437a8
emonislam33406 <mstparvinbegum8523@gmail.com>
2026-07-09 18:16:22 +0600

    Remove openGift function from script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-function openGift(){
 
-document.querySelector(".hero").style.display="none";
-
-document.getElementById("surprise").style.display="block";
-
-window.scrollTo({
-top:0,
-behavior:"smooth"
-});
-
-}
